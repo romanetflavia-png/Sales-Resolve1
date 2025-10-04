60f475af59409d3017ae83e8212996319723c60b
Flavia Romanet <romanetflavia@gmail.com>
2025-10-04 13:51:15 +0300

    Serve frontend on Render

--- server.js
+++ server.js
@@ -255,3 +255,9 @@ app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
   console.log(`Visit http://localhost:${PORT}`);
 })
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
